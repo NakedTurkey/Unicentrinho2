dd2e0ba1a8a6fa32c9b23087187c995d0bc9f6c3
NakedTurkey <110199823+NakedTurkey@users.noreply.github.com>
2023-10-25 00:32:59 -0300

    Update Atividade 4 - Ponteiros II.cpp

--- Atividade 4 - Ponteiros II.cpp
+++ Atividade 4 - Ponteiros II.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main()
 {
     int dobro(int *p);
-    void vogais(char pal, int *p);
+    void vogais(char pal[], int *p);
+    void invertStr(char pal[]);
     
         int a = 2, b = 4;//ex1
         int i;
@@ -29,10 +30,13 @@ int main()
         a = dobro(pA);
         cout << endl << a << endl;
         
-        a = 0;
+        a = 0; //ex4
         cin >> pal;
         vogais(pal, pA);
         cout << "Num de vogais: " << *pA << endl;
+        
+        cin >> pal; //ex5
+        invertStr(pal);
         
     
     return 0;
@@ -44,24 +48,69 @@ int main()
         return aux;
     }
     
-    void vogais(char pal, int *p)
+    void vogais(char pal[], int *p)
     {
-        int i, j, k = 41, l = 0;
+        int i, j, k = 65, l = 0;
             for(i = 0; i < TAM; i++)
             {
-                for(j = 0; j < 25; j++)
+                for(j = 0; j < 5; j++)
                 {
-                    if(pal[i] == k || k + 56)
+                    if(pal[i] == 'a')
                     {
-                        *p++;
-                        k = 41;
+                        *p += 1;
                         break;
                     }
-                    k++;
+                    else if(pal[i] == 'e')
+                    {
+                        *p += 1;
+                        break;
+                    }
+                    else if(pal[i] == 'i')
+                    {
+                        *p += 1;
+                        break;
+                    }
+                    else if(pal[i] == 'o')
+                    {
+                        *p += 1;
+                        break;
+                    }
+                    else if(pal[i] == 'u')
+                    {
+                        *p += 1;
+                        break;
+                    }
+                    
                 }
                 if(pal[i+1] == '\0')
                 {
                     break;
                 }
             }
+    }
+    
+    void invertStr(char pal[])
+    {
+        int i, j, k = 1, aux;
+        char *p;
+            for(i = 0; i < TAM; i++)
+            {
+                if(pal[i+1] == '\0')
+                {
+                    aux = i+1;
+                    break;
+                }
+            }
+            j = aux / 2;
+            aux--;
+            for(i = 0; j > 0; i++)
+            {
+                p = &pal[i];
+                pal[aux] = *p + pal[aux];
+                pal[i] = pal[aux] - *p;
+                pal[aux] = pal[aux] - pal[i];
+                j--;
+                aux--;
+            }
+        cout << "Pequena palavra: " << pal << endl;
     }
