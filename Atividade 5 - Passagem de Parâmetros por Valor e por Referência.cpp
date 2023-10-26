7106bf56416dc8f2e697c0f01a3458816de15081
NakedTurkey <110199823+NakedTurkey@users.noreply.github.com>
2023-10-26 09:43:54 -0300

    Update Atividade 5 - Passagem de Parâmetros por Valor e por Referência.cpp

--- Atividade 5 - Passagem de Parâmetros por Valor e por Referência.cpp
+++ Atividade 5 - Passagem de Parâmetros por Valor e por Referência.cpp
@@ -16,12 +16,61 @@ int main()
     
     
     
-    int num1, num2, vet[TAM] = {1, 2, 3, 4, 5};
-    char pal[TAM] = {"Palav"};
-    inverte(pV);
+    int num1, num2, i, vet[TAM] = {1, 2, 3, 4, 5};
+    char pal[TAM];
+    int *p1, *p2;
+    p1 = &num1;
+    p2 = &num2;
+    
+    cout << "Dois numeros: "; //ex1
+    cin >> num1 >> num2;
+    num1 = multi1(num1, num2);
+    cout << "Multipricado " << num1 << endl;
+    
+    cout << "De novo dois numerinhos: "; //ex2
+    cin >> num1 >> num2;
+    num1 = multi2(p1, p2);
+    cout << "Resurtado " << num1 << endl;
+    
+    cout << "Digite um numerin "; //ex3
+    cin >> num1;
+    num1 = soma1(num1);
+    cout << "Somado " << num1 << endl;
+    
+    cout << "Numero: "; //ex4
+    cin >> num1;
+    num1 = soma2(p1);
+    cout << "Somado " << num1 << endl;
+    
+    cout << "Numero imediatamente ";
+    cin >> num1;
+    num1 = sub1(num1);
+    cout << "Substraido " << num1 << endl; //ex5
+    
+    cout << "Numero agora: "; //ex6
+    cin >> num1;
+    num1 = sub2(p1);
+    cout << "Menos 5 " << num1 << endl;
+    
+    cout << "Encha o vetor:" << endl; //ex7
+        for(i = 0; i < TAM; i++)
+        {
+            cin >> vet[i];
+        }
+    inverte(vet);
+    cout << endl << "Invertido: ";
     for(int i = 0; i < TAM; i++)
     {
         cout << vet[i] << ' ';
+    }
+    
+    cout << endl << "Palavra: "; //ex8
+    cin >> pal;
+    maiusculo(pal);
+    cout << endl << "Maiusculada: ";
+    for(int i = 0; i < TAM; i++)
+    {
+        cout << pal[i] << ' ';
     }
     
     return 0;
@@ -71,9 +120,10 @@ void inverte(int *vet)
 
 void maiusculo(char *pal)
 {
-    int i, j, k = 0;
+    int i, j, k = 0, l;
     for(i = 0; i < TAM; i++)
     {
+        k = 0;
         for(j = 65; j <= 90; j++) //se maiuscula
         {
             if(pal[i] == j)
@@ -86,7 +136,6 @@ void maiusculo(char *pal)
         {
             continue;
         }
-        k = 0;
-        
+        pal[i] = pal[i] - 32;
     }
 }
