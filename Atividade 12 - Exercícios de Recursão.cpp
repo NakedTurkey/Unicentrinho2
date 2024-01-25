49f29b57fcfd54f9b9f20eced67915fea529503f
NakedTurkey <110199823+NakedTurkey@users.noreply.github.com>
2024-01-25 09:14:30 -0300

    Update Atividade 12 - Exercícios de Recursão.cpp

--- Atividade 12 - Exercícios de Recursão.cpp
+++ Atividade 12 - Exercícios de Recursão.cpp
@@ -1,4 +1,6 @@
-/* I. Faça teste de mesa para as duas implementações da função fatorial para as
+/* 
+ 1 -
+I. Faça teste de mesa para as duas implementações da função fatorial para as
 seguintes instâncias: 5, 3 e 0.
 
 Interativa:                     Recursiva:
@@ -6,6 +8,16 @@ Interativa:                     Recursiva:
 1   5       1   5       120         5       F        V           120
 2   3       1   3       6           3       F       V           6
 3   0       1           1           0       V       F           1
+
+II. Qual o valor de X(4)? (Feofiloff 2009)
+    O valor é 13
+
+ 2 -
+ I. Faça o teste de mesa da função max(), considerando v = {7, 12, 5, 14, 9}.
+ 
+ v              n       if1      else1     if2     else2   
+{7, 12, 5,      5       F           V       F       V
+14, 9}   
 
 
 */
