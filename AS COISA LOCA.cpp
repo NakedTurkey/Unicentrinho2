93e6e05c55be95a747dbe2f06b3c653dea113916
NakedTurkey <110199823+NakedTurkey@users.noreply.github.com>
2024-01-25 09:04:24 -0300

    Create AS COISA LOCA.cpp

---
+++ AS COISA LOCA.cpp
@@ -0,0 +1,21 @@
+/* I. Faça teste de mesa para as duas implementações da função fatorial para as
+seguintes instâncias: 5, 3 e 0.
+
+Interativa:                     Recursiva:
+    FAT     f   for     f           FAT     if1     else1      Saida 
+1   5       1   5       120         5       F        V           120
+2   3       1   3       6           3       F       V           6
+3   0       1           1           0       V       F           1
+
+
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
+
